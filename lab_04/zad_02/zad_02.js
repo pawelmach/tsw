5b2138f24c32126f6a7747c0e2990cef07660c6d
Paweł Mach <machpawel95@gmail.com>
2019-03-14 15:44:47 +0100

    created zad_02

---
+++ lab_04/zad_02/zad_02.js
@@ -0,0 +1,10 @@
+/* jshint strict: global, esversion: 6, devel: true */
+'use strict';
+
+function nbsp(tekst){
+    if(typeof(tekst) !== "string"){
+        throw({typerr: "Argument is not a string"});
+    }
+
+    
+}
